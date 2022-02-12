bf099b2e83417acb83045f583326018fd1bee86b
Ernie Schulz <82405659+ernsch777@users.noreply.github.com>
2022-02-11 23:01:16 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import autoprefixer from 'autoprefixer';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import ImageCard from './components/imageCard';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  // useEffect(() => {
+  //   fetch
+  // })
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ImageCard />
   );
 }
 
